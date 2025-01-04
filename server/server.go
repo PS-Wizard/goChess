4bb39465233b8ff50bd5d0fbac11c535c831dd24
PS-Wizard <p.swoyam.1@gmail.com>
2025-01-04 14:36:38 +0545

    Refs #1 Cleared out old HTMX stuff, fresh working ground

--- server/server.go
+++ server/server.go
@@ -2,40 +2,27 @@ package server
 
 import (
 	"fmt"
-	"goChess/board"
 	"net/http"
 )
 
 type Server struct {
-	Port   string
-	router *http.ServeMux
-	Board  *board.Board
+	Port string
 }
 
 func NewServer(port string) *Server {
 	return &Server{
-		Port:   port,
-		router: http.NewServeMux(),
+		Port: port,
 	}
 }
+
 func (s *Server) StartServer() {
-	s.router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		s.Board = board.NewBoard()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./ui/index.html")
 	})
-	s.router.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
-		peice := r.URL.Query().Get("piece")
-        from := r.URL.Query().Get("from")
-        to := r.URL.Query().Get("to")
-        old := r.URL.Query().Get("oldState")
-		new := r.URL.Query().Get("newState")
-		fmt.Println("Got Request", old, new)
-        fmt.Fprintf(w, "got request, To Move: %s , from: %s , to: %s", peice, from, to)
-        // TODO:
-        //     - Check If Move is valid,
-        //     - If It Is Its Bueno, Update Internal state
-        //     - If Not Return Html Snippet for chess.js with position of old
-	})
-	fmt.Printf("Starting a server at %s", s.Port)
-	http.ListenAndServe(s.Port, s.router)
+	fmt.Printf("Starting server at %s\n", s.Port)
+	err := http.ListenAndServe(s.Port, nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
+
